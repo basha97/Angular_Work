6c2b311aa1cf8058e312b794e2032cb7e4099e06
basha97 <bashaadil2727@gmail.com>
2019-02-15 15:04:09 +0530

    first commit

---
+++ src/app/main/pages/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { RouterModule } from '@angular/router';
+
+const routes = [
+  {
+      path     : 'home',
+      component: HomeComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    
+  ]
+})
+export class HomeModule { }
